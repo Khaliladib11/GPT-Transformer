49ffa23b1d7c5139980a2b94bab6e21d8f77fe1c
khaliladib11 <khaliladib11@gmail.com>
2023-05-29 17:58:11 +0100

    add the basic code

---
+++ src/bigram.py
@@ -0,0 +1,51 @@
+# Import libraries
+import torch
+import torch.nn as nn
+from torch.nn import functional as F
+
+
+class BigramLanguageModel(nn.Module):
+
+    def __init__(self, vocab_size):
+        super().__init__()
+        self.embedding_table = nn.Embedding(vocab_size, vocab_size)
+
+    def forward(self, context, targets=None):
+
+        # this returns a torch.tensor with shape of (BATCH_SIZE, CONTEXT_LENGTH, VOCAB_SIZE)
+        # e.g. (4, 8, 65)
+        logits = self.embedding_table(context)
+
+        if targets == None:
+            loss = None
+
+        else:
+            B, C, V = logits.shape
+
+            # we're going to strech out the array, new shape: (BATCH_SIZE * CONTEXT_LENGTH, VOCAB_SIZE)
+            # e.g. (4*8, 65) == (32, 65)
+            logits = logits.view(B * C, V)
+
+            # and for the targets as well, we're going to change it's shape to be one dim
+            # e.g. (32)
+            targets = targets.view(B * C)
+
+            loss = F.cross_entropy(logits, targets)
+
+        return logits, loss
+
+    def generate(self, context, max_tokens):
+        # Fist of all the context is with (B, C) shape
+        for _ in range(max_tokens):
+            # we get the prediction, the logits will be in (B, C, V) shape and the loss will be None
+            logits, loss = self(context)
+            # Focus only on the last character, this will change later
+            logits = logits[:, -1, :]
+            # get the probability distribuation where the sum of probabilities are equal to 1
+            probs = F.softmax(logits, dim=1)
+            # get random sample distribution from the probability
+            next_token = torch.multinomial(probs, num_samples=1)
+            # concatinate the generated token with the previous set of tokens
+            context = torch.cat((context, next_token), dim=1)
+
+        return context
---
+++ src/utils.py
@@ -0,0 +1,55 @@
+import torch
+
+
+def read_data(data_path):
+    with open(data_path, 'r') as file:
+        data = file.read()
+
+    return data
+
+
+def create_dictionaries(chars):
+    # Create two ways mapping
+    c2i = {char: idx for idx, char in enumerate(chars)}
+    i2c = {idx: char for idx, char in enumerate(chars)}
+
+    return c2i, i2c
+
+
+def split_data(data, train_percent, eval_percent, test_percent):
+    assert train_percent + eval_percent + test_percent == 1.0, f"The summation of all percentags must be 1.0, we got {train_percent + eval_percent + test_percent}"
+
+    train_range = [0, int(len(data) * train_percent)]
+    eval_range = [train_range[1], int(len(data) * eval_percent) + train_range[1]]
+    test_range = [eval_range[1], -1]
+
+    return data[:train_range[1]], data[eval_range[0]:eval_range[1]], data[test_range[0]:]
+
+
+def get_batch(data, batch_size, context_length):
+    indexes = torch.randint(len(data) - batch_size, (batch_size,))  # get BATCH_SIZE random indexes within the dataset
+
+    # loop through each index and get the context data and finally stack them together to get torch.tensor of shape (
+    # BATCH_SIZE, CONTEXT_LENGTH)
+    x = torch.stack([data[idx: idx + context_length] for idx in indexes])
+    # loop through each index and get the target data and finally stack them together to get torch.tensor of shape (
+    # BATCH_SIZE, CONTEXT_LENGTH)
+    y = torch.stack([data[idx + 1:idx + context_length + 1] for idx in indexes])
+
+    return x, y
+
+
+def training_loop(model, data, epochs, optimizer, batch_size, context_length):
+    training_loss = []
+    for epoch in range(epochs):
+        x, y = get_batch(data, batch_size, context_length)
+
+        logits, loss = model(x, y)
+        optimizer.zero_grad(set_to_none=True)
+        loss.backward()
+        optimizer.step()
+
+        training_loss.append(loss.item())
+
+    return training_loss
+
